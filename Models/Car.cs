f9a8290528371f5288e9f6fa312afe345893a3b5
Robin Wiman <X00204927@myTUDublin.ie>
2024-04-09 19:48:11 +0100

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Car
+    {
+        public string Registration { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double EngineSize { get; set; }
+        public string EngineType { get; set; } // Petrol, Diesel, Hybrid, Electric
+    }
+}
---
+++ Models/TextMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models
+{
+    public class TextMessage
+    {
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "The mobile phone number must be 10 digits.")]
+        public string MobilePhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(140, ErrorMessage = "The message content cannot exceed 140 characters.")]
+        public string Content { get; set; }
+    }
+}
